df48340492ba38355594876a3b72d3c4eecc2742
nadaa <nadaa@ajou.ac.kr>
2026-01-10 18:30:57 +0900

    learn aboud query method and Spring Data JPA

--- src/main/java/hello/springdatajpa/SpringdatajpaApplication.java
+++ src/main/java/hello/springdatajpa/SpringdatajpaApplication.java
@@ -1,9 +1,11 @@
 package hello.springdatajpa;
 
 import hello.springdatajpa.domain.User;
+import hello.springdatajpa.repository.UserRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,35 +19,41 @@ public class SpringdatajpaApplication implements CommandLineRunner {
     }
 
     @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    private UserRepository userRepository;
 
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        EntityTransaction transaction = entityManager.getTransaction();
-        try{
-            transaction.begin();
 
-            // user 추가
-//            User user = new User();
-//            user.setName("nadaa");
-//            user.setEmail("nadaa@ajou.ac.kr");
-//            user.setPassword("1234");
+        // user 추가
+//        User user = new User();
+//        user.setName("Jack");
+//        user.setEmail("jackahcak@naver.com");
+//        user.setPassword("1234");
+//        User saveUser = userRepository.save(user);
+//        System.out.println(saveUser);
+
+
+        // user 찾기
+//        User user = userRepository.findById(2).orElseThrow();
+//        System.out.println(user);
 //
-//            entityManager.persist(user);
+//
 
-            User user = entityManager.find(User.class, 1);
+        // user delete
+//        userRepository.deleteById(2);
 
-            System.out.println(user);
+        // update
+//        User user = userRepository.findById(1).orElseThrow();
+//        System.out.println(user);
+//
+//        user.setPassword("3333");
 
 
-            transaction.commit();
-        }catch (Exception e){
-            transaction.rollback();
-        }finally{
-            entityManager.close();
-        }
+        // query method
+        User user = userRepository.findByName("nadaa").orElseThrow();
+        System.out.println(user);
 
     }
 }
---
+++ src/main/java/hello/springdatajpa/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package hello.springdatajpa.repository;
+
+import hello.springdatajpa.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByName(String name);
+}
